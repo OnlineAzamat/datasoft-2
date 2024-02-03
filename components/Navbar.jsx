88045a69ee7940700fd5ffb5176ee47218929fc3
OnlineAzamat <yakubbaev.azamat@mail.ru>
2024-02-03 17:24:17 +0500

    feat: Add Hero component and Navbar component
    - Added Hero render a hero section
    - Added Navbar component to render a navigation bar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,18 @@
 'use client';
 
 import { motion } from 'framer-motion'
+import { navVariants } from '@/utils/motion';
+import styles from '@/styles/index';
 
 const Navbar = () => (
-  <nav>
-    navbar
-  </nav>
+  <motion.nav
+    variants={navVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative`}
+  >
+    <div className='absolute w-[50%] inset-0 gradient-01' />
+  </motion.nav>
 )
 
 export default Navbar;
