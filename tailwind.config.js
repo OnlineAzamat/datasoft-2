ce4fcf7385b934428d0f9db2728846c8d099b620
OnlineAzamat <yakubbaev.azamat@mail.ru>
2024-02-14 22:02:36 +0500

    added Hero page, 40:28

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,19 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./styles/**/*.{js,jsx}",
+    "./sections/**/*.{html,js,jsx}",
   ],
+  mode: 'jit',
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'primary-black': '#1a232e',
+        'secondary-white': '#c7c7c7',
       },
+      transitionTimingFunction: {
+        'out-flex': 'cubic-bezier(0.05, 0.6, 0.4, 0.9)',
+      }
     },
   },
   plugins: [],
