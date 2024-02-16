58f8140897c795e5695944af6db4b161d464c532
OnlineAzamat <yakubbaev.azamat@mail.ru>
2024-02-16 19:58:56 +0500

    added about page

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import About from "@/components/About";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 
@@ -6,6 +7,7 @@ export default function Home() {
     <div className="bg-gray-900 overflow-hidden">
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 }
