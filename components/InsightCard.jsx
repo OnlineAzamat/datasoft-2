2e7756d12b17169098e326abee4496967f071f69
OnlineAzamat <yakubbaev.azamat@mail.ru>
2024-02-21 21:40:13 +0500

    feat: add Insights component

---
+++ components/InsightCard.jsx
@@ -0,0 +1,15 @@
+'use client'
+import { motion } from "framer-motion"
+
+import { fadeIn } from "@/utils/motion"
+
+export default function InsightCard({ imgUrl, title, subtitle, index }) {
+  return (
+    <motion.div
+      variants={fadeIn('up', 'spring', index * 0.5, 1)}
+      className="flex md:flex-row flex-col gap-4"
+    >
+      <img src={imgUrl} alt="planet-01" className="md:w-[370px] w-full h-[250px] rounded-[32px] object-cover" />
+    </motion.div>
+  )
+}
